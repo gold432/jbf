aa01b5b814ba11617fe8f6f0484480d9e932619b
Gold Edem Hogan <edge37@outlook.com>
2024-07-19 11:01:47 +0100

    .

--- MetricEntry.xaml.cs
+++ MetricEntry.xaml.cs
@@ -106,7 +106,19 @@ namespace FitnessTracker
             if (double.TryParse(m1TextBox.Text, out double result)) {
                 m1 = result
             } else {
-                MessageBox.Show
+                MessageBox.Show("The first metric is not, but must be, a number")
+            }
+
+            if (double.TryParse(m2TextBox.Text, out double result)) {
+                m2 = result
+            } else {
+                MessageBox.Show("The second metric is not, but must be, a number")
+            }
+
+            if (double.TryParse(m3TextBox.Text, out double result)) {
+                m1 = result
+            } else {
+                MessageBox.Show("The third metric is not, but must be, a number")
             }
 
             _model.RecordActivity(selectedExercise, m1TextBox, m2TextBox, m3TextBox); //TODO
